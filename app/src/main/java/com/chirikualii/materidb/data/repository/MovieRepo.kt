e23ec4795307647f55ea6ee8155b255d3e6fc6e1
wahyu <201210035@student.mercubuana-yogya.ac.id>
2022-11-13 16:04:27 +0700

    commit

---
+++ app/src/main/java/com/chirikualii/materidb/data/repository/MovieRepo.kt
@@ -0,0 +1,12 @@
+package com.chirikualii.materidb.data.repository
+
+import com.chirikualii.materidb.data.model.Movie
+import retrofit2.http.Query
+
+interface MovieRepo {
+    suspend fun getPopularMovie() : List<Movie>
+    suspend fun getNowPlayingMovie() : List<Movie>
+    suspend fun getPopularMovieLocal() : List<Movie>
+    suspend fun getNowPlayingLocal(): List<Movie>
+    suspend fun getSearchMovie(query: String) : List<Movie>
+}
